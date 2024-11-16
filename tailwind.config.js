f8a34d58c7692e7f405842d6e9b27aa46ca3259f
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2024-11-15 22:19:51 -0300

    Atualizando e alterando o software

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      fontFamily: {
-        'verdana' : 'Verdana'
-      },
       colors: {
-        'btnEnter' : '#42B72A',
-        'colorLink' : '#555',
-        'btncreate' : '#0866FF'
-      },
-      spacing: {
-        '51px' : '51px',
-        '550px' : '550px',
-        '62px' : '62px',
-        '400px' : '400px',
-        '350px' : '350px'
-      },
-      screens: {
-        'min-h-900': {'raw': '(min-height: 900px)'}
+        'BgFound': '#F2F4F7',
+        'btn-Enter': '#42B72A',
+        'colorLink': '#0866FF'
       }
     },
   },
