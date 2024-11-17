8405a4d74f5079c093a241a53e2bb1165602b340
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2024-11-17 20:59:02 -0300

    colocando contagem regressiva

--- src/js2/script.js
+++ src/js2/script.js
@@ -4,6 +4,9 @@ const insertCodeBox = document.getElementById('insertCodeBox')
 const btnRecuperation = document.getElementById('btnRecuperation')
 const inputEmailRecuperation = document.getElementById('inputEmailRecuperation')
 const textoVermelho = document.getElementById('textoVermelho')
+const codeAgain = document.getElementById('codeAgain')
+const countdown = document.getElementById('countdown');
+
 // Funções
 function validationInputUser() {
 
@@ -14,3 +17,46 @@ function validationInputUser() {
 
 // Eventos
 btnRecuperation.addEventListener('click', validationInputUser)
+
+
+codeAgain.addEventListener('click', () => {
+    
+    Toastify(
+        {
+            text: "Enviamos novamente!",
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+            }
+        }
+    ).showToast()
+
+    let interval
+
+    countdown.style.display = 'block';
+    let remainingTime = 10;
+    countdown.textContent = `${remainingTime}s`;
+    countdown.style.color = 'red'
+
+    interval = setInterval(() => {
+        remainingTime -= 1
+        countdown.textContent = `${remainingTime}s`;
+        
+        if (remainingTime <= 0) {
+            // Reativar o botão e esconder a contagem
+            clearInterval(interval);
+            button.disabled = false;
+            
+          }
+    }, 1000)
+
+    codeAgain.disabled = true
+
+    setTimeout(() => {
+        codeAgain.disabled = false
+    }, 60000)
+})
